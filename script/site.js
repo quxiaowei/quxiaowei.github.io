3c0eea47e31d659837d453efe4c26002cf737ee4
quxiaowei <quxw.work@gmail.com>
2017-06-08 21:12:20 +0800

    responsive

--- script/site.js
+++ script/site.js
@@ -11,6 +11,6 @@ $().ready(
 $('#return-to-top').click(function() {      // When arrow is clicked
     $("body,html").animate({
         scrollTop : 0                       // Scroll to top of body
-    }, 500);
+    }, 200);
 });
 });
